a39fdc9aeec2788bafe08d61c533d37b8cb5ead1
Drakoun <davidboccara333@yahoo.fr>
2015-12-20 23:45:02 +0100

    Modification majeure de la classe GestionnaireDeFichiers : Méthodes : 	- getFileActuel : File , retourne le file actuel 	- setFileActuel(File) , change le file actuel si le file donné en paramètre est un dossier 	- ListeFiles() : ArrayList<File> , retourne un arrayliste de files contenu dans le fileActuel. Si le fileActuel est null retourne la liste des files depuis la racine du système 	- retourEnArriere() , change le fileActuel en son parent Remarque : pour l'ihm après avoir set ou retourEnArriere, faire un ListeFiles() pour avoir la nouvelle liste de files

--- src/main/java/modele/GestionnaireDeFichiers.java
+++ src/main/java/modele/GestionnaireDeFichiers.java
@@ -1,21 +1,39 @@
 package modele;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GestionnaireDeFichiers {
 	
-	private File[] roots;
-	
-	public void GestionnaireDeFichiers() {
-		this.roots = File.listRoots();
-	}
-	
-	public File[] listeFile(File f) {
-		if (f.isDirectory()){
-			return f.listFiles();
-		}
-		return null;
-	}
-	
+    private File fileActuel;
+
+    public void GestionnaireDeFichiers() {
+        this.fileActuel = null;
+    }
+    
+    public void setFileActuel(File f) {
+        if (f.isDirectory()){
+            this.fileActuel = f;
+        }
+    }
+    
+    public File getFileActuel() {
+        return this.fileActuel;
+    }
+
+    public ArrayList<File> ListeFiles() {
+        // cas du démarrage du programme, on démarre sur la racine indépendamment du système
+        if (fileActuel == null) {
+            return new ArrayList<File>(Arrays.asList(File.listRoots()));
+        }
+        else {
+            return new ArrayList<File>(Arrays.asList(fileActuel.listFiles()));
+        }
+    }
+    
+    public void retourEnArriere() {
+        this.fileActuel = this.fileActuel.getParentFile();
+    }
 	
 }
